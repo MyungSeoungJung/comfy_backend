a7f2c82268ba7a857f471bf3bf69f4a8cdf078d2
MyungSeoungJung <audtmdwjd@naver.com>
2024-02-21 17:48:37 +0900

    create signIn

--- src/main/java/com/comfy_backend/auth/util/Hash.java
+++ src/main/java/com/comfy_backend/auth/util/Hash.java
@@ -10,4 +10,11 @@ public class Hash {
             .withDefaults()
             .hashToString(12, cipherText.toCharArray());
     }
+
+    public boolean verifyHash (String cipherText, String hash){
+        return BCrypt
+                .verifyer()
+                .verify(cipherText.toCharArray(), hash)
+                .verified;
+    }
 }
--- src/main/java/com/comfy_backend/user/entity/UserRepository.java
+++ src/main/java/com/comfy_backend/user/entity/UserRepository.java
@@ -2,7 +2,11 @@ package com.comfy_backend.user.entity;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
 @Primary
 public interface UserRepository extends JpaRepository<User,Long> {
 
+    Optional<User> findByEmail(String email);
 }
