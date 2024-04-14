81588df5b20008c717d6bbf9b99755d5b375e155
MyungSeoungJung <audtmdwjd@naver.com>
2024-04-14 21:12:36 +0900

    채팅 로그 가져오기

--- src/main/java/com/comfy_backend/chat/controller/MessageController.java
+++ src/main/java/com/comfy_backend/chat/controller/MessageController.java
@@ -4,6 +4,7 @@ package com.comfy_backend.chat.controller;
 import com.comfy_backend.auth.Auth;
 import com.comfy_backend.auth.AuthProfile;
 import com.comfy_backend.chat.dto.ChatListResponseDto;
+import com.comfy_backend.chat.dto.ChatLogResponseDto;
 import com.comfy_backend.chat.dto.ChatRequestDto;
 import com.comfy_backend.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
@@ -12,9 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -49,5 +48,11 @@ public class MessageController {
     public ChatListResponseDto chatList (@RequestAttribute AuthProfile authProfile){
 
         return chatService.getList(authProfile.getId());
+    }
+
+    @PostMapping("/chat/getChatLog")
+    public ChatLogResponseDto getRoom(@RequestBody ChatRequestDto dto) {
+        System.out.println("로그 조회성공");
+        return chatService.getRoom(dto.getRoomId());
     }
 }
